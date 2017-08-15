f16002b8664f3fb5bb7dbb761f85b9b2da1740a8
Alessandro Muraro <a.muraro@navabi.de>
2017-08-15 15:01:54 +0200

    fix babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './assets/js/src/main.js',
+  entry: './assets/js/dist/main.babel.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'assets'),
     filename: 'js/dist/main.bundle.js',
   },
   devtool: 'source-map'
